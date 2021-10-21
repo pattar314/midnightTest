d677a877ac014814212221acf2fa32d7672e88e2
Patrick Thurston <patsdroid18@gmail.com>
2021-10-21 02:15:08 -0600

    modified onClick to add capital C

--- src/client/components/ContactPage.js
+++ src/client/components/ContactPage.js
@@ -63,18 +63,18 @@ const ContactPage = () => {
                 <b><h3 className='form-label'>Name</h3></b><br/>
                 <div className='name-input-group'>
                     <div className='another-name-class'>
-                        <input type='text' id='first-name-text' name='firstName' className='inputs' onChange={handleChange('firstName')} value={formData['firstName']} />
+                        <input type='text' id='first-name-text' name='firstName' onChange={handleChange('firstName')} value={formData['firstName']} />
                         <label htmlFor='first-name' className='sub-label'>First</label>
                         </div>                
                     <div className='another-name-class'>
-                        <input type='text' id='last-name-text' name='lastName' className='inputs' onChange={handleChange("lastName")} value={formData['lastName']} />
+                        <input type='text' id='last-name-text' name='lastName' onChange={handleChange("lastName")} value={formData['lastName']} />
                         <label htmlFor='last-name' className='sub-label'>Last</label><br/>  
                 </div></div>
 
                 <label htmlFor='email' className='form-label'><b><h3>Email</h3></b></label><br/>
-                <input type='email' name='email' id='email-text' className='inputs' onChange={handleChange("email")} value={formData['email']} /><br/>
+                <input type='email' name='email' id='email-text' onChange={handleChange("email")} value={formData['email']} /><br/>
                 <label htmlFor='message' className='form-label'><b><h3>Comment or Message</h3></b></label><br/>
-                <textarea name='message' id='message-text' className='inputs' rows='10' cols='30' onChange={handleChange("message")} value={formData['message']} />
+                <textarea name='message' id='message-text' rows='10' cols='30' onChange={handleChange("message")} value={formData['message']} />
                 <button type='submit' >Submit</button>
             </form>
         </div>
